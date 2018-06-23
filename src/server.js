e6f405dcb4054e2c72c7dc405e1ed082192120d5
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-06-23 17:35:34 +0200

    Setup Socket.io

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 import app from './app';
-
+import socket from  'socket.io';
 
 app.set('port', process.env.PORT || 8080);
 
 const server = app.listen(app.get('port'), () => {
     console.log(`Listening on ${server.address().port}`);
+});
+
+const io = socket(server);
+
+io.on('connection', socket => {
+    console.log('Hey, I am working now! Do not disturb!');
 });
