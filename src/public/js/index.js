558f5abba4afa3f0e908fd18e98f7481eb36572d
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-07-08 19:17:02 +0200

    Blocking sending empty message | Clear message box after send | scroll message box

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -4,6 +4,7 @@ import '../scss/main.scss';
 const message = document.getElementById('message');
 const btn = document.getElementById('send');
 const output = document.getElementById('output');
+const scrollContainer = document.querySelector('.messages');
 
 btn.addEventListener('click', function(){
     socket.emit('chat', {
@@ -13,5 +14,8 @@ btn.addEventListener('click', function(){
 });
 
 socket.on('chat', function(data) {
+    if(!message.value) return;
     output.innerHTML += `<li><strong>${data.nickname}:</strong>${data.message}</li>`;
+    message.value = '';
+    scrollContainer.scrollTo(0,output.scrollHeight);
 });
