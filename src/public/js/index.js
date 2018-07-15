b4e01e196c14b4d96a12abbfc46e786b3dfb0e0a
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-07-15 13:40:12 +0200

    Set random color nickname to user

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -6,21 +6,34 @@ const btn = document.getElementById('send');
 const output = document.getElementById('output');
 const scrollContainer = document.querySelector('.messages');
 
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+const color = getRandomColor();
+
 btn.addEventListener('click', function() {
+
     if(!messageField.value) return;
     const message = messageField.value;
     messageField.value = '';
     socket.emit('chat', {
         message: message,
         nickname: nickname,
-        room: room
+        room: room,
+        color: color
     });
 
 });
 
 socket.on('chat', function(data) {
     if(data.room === room) {
-        output.innerHTML += `<li><strong>${data.nickname}:</strong>${data.message}</li>`;
+        output.innerHTML += `<li><strong style='color:${data.color}'>${data.nickname}:</strong>${data.message}</li>`;
         scrollContainer.scrollTo(0,data.scroll);
     }
 });
